4fd205e98a3138504fdbfc7a896f9effa9181e96
klwoodward <klwoodward99@yahoo.com>
2026-03-06 09:38:11 -0800

    fixed full matches extraction, added load to local database

--- etl_project/assets/football.py
+++ etl_project/assets/football.py
@@ -1,7 +1,10 @@
 from etl_project.connectors.football_api import FootballDataAPI 
+from etl_project.connectors.postgresql import PostgreSqlClient
 import time
 from loguru import logger
-from datetime import datetime, timezone
+from sqlalchemy import Table, MetaData
+import pandas as pd
+#from datetime import datetime, timezone
 
 
 def extract_competitions(football_api_client: FootballDataAPI):
@@ -17,19 +20,70 @@ def extract_competitions(football_api_client: FootballDataAPI):
 
     return comp_ids
 
-def extract_matches_full(football_api_client: FootballDataAPI, comp_ids: list[int]): #, min_datetime: str
+def extract_matches_full(football_api_client: FootballDataAPI, comp_ids: list[int]): #, min_datetime: str ???
     """
     Perform extraction of all matches for competitions, for a list of competitions
     """
-
-
 
     logger.info("The free API only allows 10 calls per minute, so we will stagger our calls...")
 
     matches = []
     for comp_id in comp_ids:
-        matches.append(football_api_client.get_matches(competition_id = comp_id))
         logger.info(f"Getting match data for comp_id:{comp_id}")
+        matches.extend(football_api_client.get_matches(competition_id = comp_id))
         time.sleep(7) #the free api only allows 10 calls per minute, so we will stagger our calls
 
-    return matches
+    df = pd.json_normalize(matches, sep="_")
+    return df
+
+
+
+def transform(df_football: pd.DataFrame) -> pd.DataFrame:
+    """Transform the raw dataframes."""
+
+    columns_to_drop = [
+        "referees",
+        "odds_msg",
+        "homeTeam_crest",
+        "competition_emblem",
+        "awayTeam_crest"
+    ]
+
+    df_transformed = df_football.drop(columns=columns_to_drop, errors="ignore")
+
+    return df_transformed
+
+
+def load(
+    df: pd.DataFrame,
+    postgresql_client: PostgreSqlClient,
+    table: Table,
+    metadata: MetaData,
+    load_method: str = "overwrite",
+) -> None:
+    """
+    Load dataframe to either a database.
+
+    Args:
+        df: dataframe to load
+        postgresql_client: postgresql client
+        table: sqlalchemy table
+        metadata: sqlalchemy metadata
+        load_method: supports one of: [insert, upsert, overwrite]
+    """
+    if load_method == "insert":
+        postgresql_client.insert(
+            data=df.to_dict(orient="records"), table=table, metadata=metadata
+        )
+    elif load_method == "upsert":
+        postgresql_client.upsert(
+            data=df.to_dict(orient="records"), table=table, metadata=metadata
+        )
+    elif load_method == "overwrite":
+        postgresql_client.overwrite(
+            data=df.to_dict(orient="records"), table=table, metadata=metadata
+        )
+    else:
+        raise Exception(
+            "Please specify a correct load method: [insert, upsert, overwrite]"
+        )
--- etl_project/pipelines/football.py
+++ etl_project/pipelines/football.py
@@ -1,7 +1,11 @@
 from etl_project.connectors.football_api import FootballDataAPI 
+from etl_project.connectors.postgresql import PostgreSqlClient
+from sqlalchemy import Table, MetaData, Column, Integer, String, Float
 from etl_project.assets.football import (
     extract_competitions,
-    extract_matches_full
+    extract_matches_full,
+    transform,
+    load
 )
 from loguru import logger
 from dotenv import load_dotenv
@@ -17,20 +21,91 @@ if __name__ == "__main__":
     logger.info("Starting pipeline run")
 
     logger.info("Getting pipeline environment variables")
+    # environment variables for api
     API_TOKEN = os.environ.get("API_TOKEN")
-    # DB_USERNAME = os.environ.get("DB_USERNAME")
-    # DB_PASSWORD = os.environ.get("DB_PASSWORD")
-    # SERVER_NAME = os.environ.get("SERVER_NAME")
-    # DATABASE_NAME = os.environ.get("DATABASE_NAME")
-    # PORT = os.environ.get("PORT")
+
+    # environment variables for database
+    DB_USERNAME = os.environ.get("DB_USERNAME")
+    DB_PASSWORD = os.environ.get("DB_PASSWORD")
+    SERVER_NAME = os.environ.get("SERVER_NAME")
+    DATABASE_NAME = os.environ.get("DATABASE_NAME")
+    PORT = os.environ.get("PORT")
 
     logger.info("Creating Football API Client")
     football_api_client = FootballDataAPI(api_token=API_TOKEN)
 
     logger.info("Extracting data from Football API")
-    comp_ids = extract_competitions(football_api_client=football_api_client)
-    match_data = extract_matches_full(football_api_client=football_api_client, comp_ids=comp_ids)
-    logger.info("Data extracted from Football API")
+    #comp_ids = extract_competitions(football_api_client=football_api_client)
+    comp_ids = [2016]
+    df_football = extract_matches_full(football_api_client=football_api_client, comp_ids=comp_ids)
 
-    print("Competition IDs:", comp_ids)
-    print(len(match_data))
+    logger.info("Transforming dataframe")
+    df_transformed = transform(df_football=df_football)
+
+    # load
+    logger.info("Loading data to postgres")
+    postgresql_client = PostgreSqlClient(
+        server_name=SERVER_NAME,
+        database_name=DATABASE_NAME,
+        username=DB_USERNAME,
+        password=DB_PASSWORD,
+        port=PORT,
+    )
+
+    metadata = MetaData()
+    table = Table(
+        "football_data",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("utcDate", String),
+        Column("status", String),
+        Column("matchday", Integer),
+        Column("stage", String),
+        Column("group", String),
+        Column("lastUpdated", String),
+        #Column("referees", String),
+        Column("area_id", Integer),
+        Column("area_name", String),
+        Column("area_code", String),
+        Column("area_flag", String),
+        Column("competition_id", Integer),
+        Column("competition_name", String),
+        Column("competition_code", String),
+        Column("competition_type", String),
+        #Column("competition_emblem", String),
+        Column("season_id", Integer),
+        Column("season_startDate", String),
+        Column("season_endDate", String),
+        Column("season_currentMatchday", Integer),
+        Column("season_winner", String),
+        Column("homeTeam_id", Integer),
+        Column("homeTeam_name", String),
+        Column("homeTeam_shortName", String),
+        Column("homeTeam_tla", String),
+        #Column("homeTeam_crest", String),
+        Column("awayTeam_id", Integer),
+        Column("awayTeam_name", String),
+        Column("awayTeam_shortName", String),
+        Column("awayTeam_tla", String),
+        #Column("awayTeam_crest", String),
+        Column("score_winner", String),
+        Column("score_duration", String),
+        Column("score_fullTime_home", Float),
+        Column("score_fullTime_away", Float),
+        Column("score_halfTime_home", Float),
+        Column("score_halfTime_away", Float)
+        #Column("odds_msg", String)
+    )
+
+    load(
+        df=df_transformed,
+        postgresql_client=postgresql_client,
+        table=table,
+        metadata=metadata,
+        load_method="insert",
+    )
+
+    logger.success("Pipeline run successful")
+
+    # print("Competition IDs:", comp_ids)
+    # print(len(match_data))
