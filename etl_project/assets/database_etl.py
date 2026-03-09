8cf15c502f8cea3e500259d47dd79960fa08ea27
klwoodward <klwoodward99@yahoo.com>
2026-03-08 23:05:37 -0700

    incremental extract fixes: needed to add a nanosecond to the max incremental value before passing it to api as query parameter, and needed to handle case where API returns no new records
    dummy transformation sql files, and basic dag functionality

--- etl_project/assets/database_etl.py
+++ etl_project/assets/database_etl.py
@@ -1,38 +1,38 @@
 from jinja2 import Environment, Template
-from etl_project.assets.database_extractor import (
-    SqlExtractParser,
-    DatabaseTableExtractor
-)
+# from etl_project.assets.database_extractor import (
+#     SqlExtractParser,
+#     DatabaseTableExtractor
+# )
 from etl_project.connectors.postgresql import PostgreSqlClient
 from graphlib import TopologicalSorter
 import pandas as pd
 from sqlalchemy import Table, MetaData
 
 
-def extract_load(
-    template_environment: Environment,
-    source_postgresql_client: PostgreSqlClient,
-    target_postgresql_client: PostgreSqlClient,
-):
-    """
-    Perform data extraction specified in a jinja template_environment.
+# def extract_load(
+#     template_environment: Environment,
+#     source_postgresql_client: PostgreSqlClient,
+#     target_postgresql_client: PostgreSqlClient,
+# ):
+#     """
+#     Perform data extraction specified in a jinja template_environment.
 
-    Data is extracted using a source_postgresql_client, and loaded using a target_postgresql_client.
-    """
-    for asset in template_environment.list_templates():
-        sql_extract_parser = SqlExtractParser(
-            file_path=asset, environment=template_environment
-        )
-        database_table_extractor = DatabaseTableExtractor(
-            sql_extract_parser=sql_extract_parser,
-            source_postgresql_client=source_postgresql_client,
-            target_postgresql_client=target_postgresql_client,
-        )
-        table_schema, metadata = database_table_extractor.get_table_schema()
-        table_data = database_table_extractor.extract()
-        target_postgresql_client.upsert_in_chunks(
-            data=table_data, table=table_schema, metadata=metadata
-        )
+#     Data is extracted using a source_postgresql_client, and loaded using a target_postgresql_client.
+#     """
+#     for asset in template_environment.list_templates():
+#         sql_extract_parser = SqlExtractParser(
+#             file_path=asset, environment=template_environment
+#         )
+#         database_table_extractor = DatabaseTableExtractor(
+#             sql_extract_parser=sql_extract_parser,
+#             source_postgresql_client=source_postgresql_client,
+#             target_postgresql_client=target_postgresql_client,
+#         )
+#         table_schema, metadata = database_table_extractor.get_table_schema()
+#         table_data = database_table_extractor.extract()
+#         target_postgresql_client.upsert_in_chunks(
+#             data=table_data, table=table_schema, metadata=metadata
+#         )
 
 
 class SqlTransform:
@@ -60,7 +60,7 @@ class SqlTransform:
         self.postgresql_client.execute_sql(exec_sql)
 
 
-def transform(dag: TopologicalSorter):
+def dag_transform(dag: TopologicalSorter):
     """
     Performs `create table as` on all nodes in the provided DAG.
     """
@@ -84,7 +84,7 @@ def extract_max_incremental(
 
     sql = f"""
         select max({jinja_config.get("incremental_column")}) as incremental_value
-        from {jinja_config.get("source_table_name")}
+        from {jinja_config.get("table_name")}
     """
     sql_response = postgresql_client.run_sql(sql)
-    sql_response[0].get("incremental_value")
+    return sql_response[0].get("incremental_value")
---
+++ etl_project/assets/sql/alpaca/transform/alpaca_stats_2.sql
@@ -0,0 +1,5 @@
+select 
+    *,
+    2 AS b
+from alpaca_stats_1
+
